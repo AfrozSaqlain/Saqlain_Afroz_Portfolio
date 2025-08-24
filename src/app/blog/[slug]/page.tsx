8b19ec2a182aa355a25e86043a1cb3b9a94b3dd6
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2025-08-25 00:14:32 +0530

    Thirteenth Commit

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -72,7 +72,7 @@ export default async function Blog({
               image: `${baseURL}${person.avatar}`,
             }}
           />
-          <Button data-border="rounded" href="/blog" weight="default" variant="tertiary" size="s" prefixIcon="chevronLeft">
+          <Button data-border="rounded" href="/blog" weight="default" variant="tertiary" prefixIcon="chevronLeft">
             Posts
           </Button>
           <Heading variant="display-strong-s">{post.metadata.title}</Heading>
