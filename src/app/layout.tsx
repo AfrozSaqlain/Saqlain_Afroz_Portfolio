194653f13778a3ae082356f1b042554f73e66d9b
Saqlain Afroz <sa20ms230@iiserkol.ac.in>
2025-08-25 00:05:24 +0530

    Twelveth Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import classNames from "classnames";
 import { Background, Column, Flex, Meta, opacity, SpacingToken } from "@once-ui-system/core";
 import { Footer, Header, RouteGuard, Providers } from '@/components';
 import { baseURL, effects, fonts, style, dataStyle, home } from '@/resources';
+import { Analytics } from "@vercel/analytics/next"
 import "katex/dist/katex.min.css";
 
 export async function generateMetadata() {
@@ -158,9 +159,10 @@ export default async function RootLayout({
               flex={1}
             >
               <Flex horizontal="center" fillWidth minHeight="0">
-                <RouteGuard>
-                  {children}
-                </RouteGuard>
+                  <RouteGuard>
+                    {children}
+                  </RouteGuard>
+                <Analytics />
               </Flex>
             </Flex>
             <Footer/>
