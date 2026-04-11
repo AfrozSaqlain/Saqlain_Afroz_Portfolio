39c83a2e3824f8ed7cc89f52eef4958fda4240cd
Saqlain Afroz <sqn3680@gmail.com>
2026-04-11 20:07:08 +0530

    Updated email and saved a commented part for projects page

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -97,3 +97,122 @@ export default async function Project({
     </Column>
   );
 }
+
+
+
+
+// import { notFound } from "next/navigation";
+// import { getPosts } from "@/utils/utils";
+// import { Meta, Schema, AvatarGroup, Button, Column, Flex, Heading, HeadingNav, Icon, Media, Row, Text } from "@once-ui-system/core";
+// import { baseURL, about, person, work } from "@/resources";
+// import { formatDate } from "@/utils/formatDate";
+// import { ScrollToHash, CustomMDX } from "@/components";
+// import { Metadata } from "next";
+
+// export async function generateStaticParams(): Promise<{ slug: string }[]> {
+//   const posts = getPosts(["src", "app", "work", "projects"]);
+//   return posts.map((post) => ({
+//     slug: post.slug,
+//   }));
+// }
+
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: Promise<{ slug: string | string[] }>;
+// }): Promise<Metadata> {
+//   const routeParams = await params;
+//   const slugPath = Array.isArray(routeParams.slug) ? routeParams.slug.join('/') : routeParams.slug || '';
+
+//   const posts = getPosts(["src", "app", "work", "projects"])
+//   let post = posts.find((post) => post.slug === slugPath);
+
+//   if (!post) return {};
+
+//   return Meta.generate({
+//     title: post.metadata.title,
+//     description: post.metadata.summary,
+//     baseURL: baseURL,
+//     image: post.metadata.image || `/api/og/generate?title=${post.metadata.title}`,
+//     path: `${work.path}/${post.slug}`,
+//   });
+// }
+
+// export default async function Project({
+//   params
+// }: { params: Promise<{ slug: string | string[] }> }) {
+//   const routeParams = await params;
+//   const slugPath = Array.isArray(routeParams.slug) ? routeParams.slug.join('/') : routeParams.slug || '';
+
+//   let post = getPosts(["src", "app", "work", "projects"]).find((post) => post.slug === slugPath);
+
+//   if (!post) {
+//     notFound();
+//   }
+
+//   const avatars =
+//     post.metadata.team?.map((person) => ({
+//       src: person.avatar,
+//     })) || [];
+
+//   return (
+//     <Row fillWidth>
+//       <Row maxWidth={12} hide="m" />
+//       <Row fillWidth horizontal="center">
+//         <Column as="section" maxWidth="xs" gap="l">
+//           <Schema
+//             as="blogPosting"
+//             baseURL={baseURL}
+//             path={`${work.path}/${post.slug}`}
+//             title={post.metadata.title}
+//             description={post.metadata.summary}
+//             datePublished={post.metadata.publishedAt}
+//             dateModified={post.metadata.publishedAt}
+//             image={post.metadata.image || `/api/og/generate?title=${encodeURIComponent(post.metadata.title)}`}
+//             author={{
+//               name: person.name,
+//               url: `${baseURL}${about.path}`,
+//               image: `${baseURL}${person.avatar}`,
+//             }}
+//           />
+//           <Button data-border="rounded" href="/work" variant="tertiary" prefixIcon="chevronLeft">
+//             Projects
+//           </Button>
+//           <Heading variant="display-strong-s">{post.metadata.title}</Heading>
+//           {post.metadata.images.length > 0 && (
+//             <Media
+//               priority
+//               aspectRatio="16 / 9"
+//               radius="m"
+//               alt="image"
+//               src={post.metadata.images[0]}
+//             />
+//           )}
+//           <Flex gap="12" marginBottom="24" vertical="center">
+//             {post.metadata.team && <AvatarGroup reverse avatars={avatars} size="m" />}
+//             <Text variant="body-default-s" onBackground="neutral-weak">
+//               {post.metadata.publishedAt && formatDate(post.metadata.publishedAt)}
+//             </Text>
+//           </Flex>
+//           <Column as="article" fillWidth>
+//             <CustomMDX source={post.content} />
+//           </Column>
+//           <ScrollToHash />
+//         </Column>
+//       </Row>
+//       <Column maxWidth={18} paddingLeft="40" fitHeight position="sticky" top="80" gap="20" hide="m">
+//         <Row
+//           gap="12"
+//           paddingLeft="2"
+//           vertical="center"
+//           onBackground="neutral-medium"
+//           textVariant="label-default-s"
+//         >
+//           <Icon name="document" size="xs" />
+//           On this page
+//         </Row>
+//         <HeadingNav fitHeight />
+//       </Column>
+//     </Row>
+//   );
+// }
